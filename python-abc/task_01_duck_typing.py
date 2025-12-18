b70dde059dfe9363c68d9192fa77d5faf6c2a49b
Reef Saeed <reefsaeed@hotmail.com>
2025-12-18 23:38:19 +0000

    git add .!

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -1,41 +1,83 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+
+"""
+Duck typing - an object-oriented programming term used to refer to the application of
+"""
+
 from abc import ABC, abstractmethod
-import math
 
 
 class Shape(ABC):
+    """
+    Class Shape
+    """
     @abstractmethod
     def area(self):
+        """
+        area method
+        """
         pass
 
     @abstractmethod
     def perimeter(self):
+        """
+        perimeter method
+        """
         pass
 
 
 class Circle(Shape):
+    """
+    Class Circle
+    """
+
     def __init__(self, radius):
-        self.radius = radius
+        """
+        Initializes a new Circle instance.
+        """
+        self.__radius = abs(radius)
 
     def area(self):
-        return math.pi * (self.radius ** 2)
+        """
+        area method
+        """
+        return 3.141592653589793 * self.__radius ** 2
 
     def perimeter(self):
-        return 2 * math.pi * self.radius
+        """
+        perimeter method
+        """
+        return 2 * 3.141592653589793 * self.__radius
 
 
 class Rectangle(Shape):
+    """
+    Class Rectangle
+    """
+
     def __init__(self, width, height):
-        self.width = width
-        self.height = height
+        """
+        Initializes a new Rectangle instance.
+        """
+        self.__width = width
+        self.__height = height
 
     def area(self):
-        return self.width * self.height
+        """
+        area method
+        """
+        return self.__width * self.__height
 
     def perimeter(self):
-        return 2 * (self.width + self.height)
+        """
+        perimeter method
+        """
+        return 2 * (self.__width + self.__height)
 
 
 def shape_info(shape):
-    print(f"Area: {shape.area()}")
-    print(f"Perimeter: {shape.perimeter()}")
+    """
+    shape_info function
+    """
+    print("Area: {}".format(shape.area()))
+    print("Perimeter: {}".format(shape.perimeter()))
