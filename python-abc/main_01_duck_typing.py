1f1a451e44a462ebfdb269b9b82cd4cee3b8c882
Reef Saeed <reefsaeed@hotmail.com>
2025-12-18 22:53:57 +0000

    update

--- python-abc/main_01_duck_typing.py
+++ python-abc/main_01_duck_typing.py
@@ -1,24 +1,8 @@
 #!/usr/bin/env python3
-"""Test file for duck typing shapes."""
 from task_01_duck_typing import Circle, Rectangle, shape_info
 
-print("Testing Circle with radius=5:")
 circle = Circle(radius=5)
-print("Circle created")
-print("Circle area:", circle.area())
-print("Circle perimeter:", circle.perimeter())
-print()
-
-print("Testing Rectangle with width=4, height=7:")
 rectangle = Rectangle(width=4, height=7)
-print("Rectangle created")
-print("Rectangle area:", rectangle.area())
-print("Rectangle perimeter:", rectangle.perimeter())
-print()
 
-print("Testing shape_info with Circle:")
 shape_info(circle)
-print()
-
-print("Testing shape_info with Rectangle:")
 shape_info(rectangle)
