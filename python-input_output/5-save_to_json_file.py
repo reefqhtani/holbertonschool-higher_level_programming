3ef9da7ff4baa4779e78c18707fcb2f195080f7a
Reef Saeed <reefsaeed@hotmail.com>
2025-12-22 15:50:30 +0000

    Add save_to_json_file function for task 5

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Save a Python object to a file using JSON representation."""
+
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """Write a Python object to a text file using JSON."""
+    with open(filename, "w", encoding="utf-8") as f:
+        json.dump(my_obj, f)
